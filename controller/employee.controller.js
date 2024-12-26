2699fb265b909bffc591428b47e6144a1b6ccbe8
achal <achal@gmail.com>
2024-12-26 08:47:58 +0530

    profile change

--- controller/employee.controller.js
+++ controller/employee.controller.js
@@ -5,7 +5,7 @@ exports.readEmployeeTodo = asyncHandler(async (req, res) => {
     const result = await Todo.find({ employee: req.employee })
     res.json({ message: "todo fetch success", result })
 })
-exports.completeEmployeeTodo = asyncHandler(async () => {
+exports.completeEmployeeTodo = asyncHandler(async (req, res) => {
     await Todo.findByIdAndUpdate(req.params.tid, { isComplete: true })
     res.json({ message: "todo complete success" })
 })
